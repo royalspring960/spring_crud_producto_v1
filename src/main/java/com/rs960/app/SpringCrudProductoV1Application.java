d643a9f3ce971fa94bd48fd6d61aea72560c8990
Royal Spring <royalspring.app@gmail.com>
2026-04-26 14:25:39 -0400

    Add project empty

---
+++ src/main/java/com/rs960/app/SpringCrudProductoV1Application.java
@@ -0,0 +1,13 @@
+package com.rs960.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrudProductoV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCrudProductoV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/rs960/app/SpringCrudProductoV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rs960.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrudProductoV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
